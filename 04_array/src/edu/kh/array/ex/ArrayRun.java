bcb33cb2b1d86ffd7e61fe018a5f834da86f3b5e
otterWak <erichonggu@gmail.com>
2024-09-02 17:11:12 +0900

    20240902

---
+++ 04_array/src/edu/kh/array/ex/ArrayRun.java
@@ -0,0 +1,13 @@
+package edu.kh.array.ex;
+
+public class ArrayRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ArrayExample1 AE1 = new ArrayExample1();
+		
+		AE1.ex1();
+	}
+
+}
