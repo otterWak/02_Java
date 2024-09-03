e5afff8efa782b3c14d104daba9387ec8b828df7
otterWak <erichonggu@gmail.com>
2024-09-03 17:47:16 +0900

    20240903 배열

--- 04_array/src/edu/kh/array/ex/ArrayRun.java
+++ 04_array/src/edu/kh/array/ex/ArrayRun.java
@@ -6,8 +6,21 @@ public class ArrayRun {
 		// TODO Auto-generated method stub
 
 		ArrayExample1 AE1 = new ArrayExample1();
+		ArrayExample2 AE2 = new ArrayExample2();
 		
-		AE1.ex1();
+//		AE1.ex1();
+//		AE1.ex2();
+//		AE1.ex3();
+//		AE1.ex4();
+//		AE1.ex5();
+//		AE1.ex6();
+//		AE1.ex7();
+//		AE1.ex8();
+//		AE1.ex9();
+		
+//		AE2.shallowCopy();
+//		AE2.deeaCopy();
+		AE2.createLottoNumber();
 	}
 
 }
---
+++ 04_array/src/edu/kh/array/practice/run/PracticeRun.java
@@ -0,0 +1,35 @@
+package edu.kh.array.practice.run;
+
+import edu.kh.array.practice.service.PracticeService;
+
+public class PracticeRun {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		PracticeService PS = new PracticeService();
+		
+//		PS.practice1();
+//		PS.practice2();
+//		PS.practice3();
+//		PS.practice4();
+//		PS.practice5();
+//		PS.practice6();
+//		PS.practice7();
+//		PS.practice8();
+//		PS.practice9();
+//		PS.practice10();
+//		PS.practice11();
+//		PS.practice12();
+		PS.practice13();
+//		PS.practice14();
+//		
+//		8번 출력 화면 모르겠음
+//		13번 문자 출력 모르겠음
+//		14번 너무 어려움
+//		
+		
+		
+	}
+
+}
